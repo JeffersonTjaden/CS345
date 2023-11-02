5229d65391b3d5a1df3f8140da913deaefbe29d0
hooncx <148787816+hooncx@users.noreply.github.com>
2023-11-02 17:12:15 -0400

    Update Calculator.java

--- src/GUI/Calculator.java
+++ src/GUI/Calculator.java
@@ -155,86 +155,112 @@ public class Calculator extends JFrame implements ActionListener
     content.add(back, c);
 
     add = new JButton("+");
+    add.setActionCommand(actionCommand:"add");
     add.addActionListener(this);
     c.gridx = (3);
     c.gridy = (3);
     content.add(add, c);
 
     sign = new JButton(Character.toString((char) 177));
+    sign.setActionCommand(actionCommand:"sign");
+    sign.addActionListener(this);
     c.gridx = (4);
     c.gridy = (3);
     content.add(sign, c);
 
     seven = new JButton("7");
+    seven.setActionCommand(actionCommand:"seven");
+    seven.addActionListener(this);
     c.gridx = 0;
     c.gridy = 4;
     content.add(seven, c);
 
     eight = new JButton("8");
+    eight.setActionCommand(actionCommand:"eight");
+    eight.addActionListener(this);
     c.gridx = 1;
     c.gridy = 4;
     content.add(eight, c);
 
     nine = new JButton("9");
+    nine.setActionCommand(actionCommand:"nine");
+    nine.addActionListener(this);
     c.gridx = 2;
     c.gridy = 4;
     content.add(nine, c);
 
     minus = new JButton("-");
+    minus.setActionCommand(actionCommand:"minus");
     minus.addActionListener(this);
     c.gridx = 3;
     c.gridy = 4;
     content.add(minus, c);
 
     four = new JButton("4");
+    four.setActionCommand(actionCommand:"four");
+    four.addActionListener(this);
     c.gridx = 0;
     c.gridy = 5;
     content.add(four, c);
 
     five = new JButton("5");
+    five.setActionCommand(actionCommand:"five");
+    five.addActionListener(this);
     c.gridx = 1;
     c.gridy = 5;
     content.add(five, c);
 
     six = new JButton("6");
+    six.setActionCommand(actionCommand:"six");
+    six.addActionListener(this);
     c.gridx = 2;
     c.gridy = 5;
     content.add(six, c);
 
     multiply = new JButton("X");
+    multiply.setActionCommand(actionCommand:"divide");
     multiply.addActionListener(this);
     c.gridx = 3;
     c.gridy = 5;
     content.add(multiply, c);
 
     one = new JButton("1");
+    one.setActionCommand(actionCommand:"one");
+    one.addActionListener(this);
     c.gridx = 0;
     c.gridy = 6;
     content.add(one, c);
 
     two = new JButton("2");
+    two.setActionCommand(actionCommand:"two");
+    two.addActionListener(this);
     c.gridx = 1;
     c.gridy = 6;
     content.add(two, c);
 
     three = new JButton("3");
+    three.setActionCommand(actionCommand:"three");
+    three.addActionListener(this);
     c.gridx = 2;
     c.gridy = 6;
     content.add(three, c);
 
     divide = new JButton(Character.toString((char) 247));
+    divide.setActionCommand(actionCommand:"divide");
     divide.addActionListener(this);
     c.gridx = 3;
     c.gridy = 6;
     content.add(divide, c);
 
     bar = new JButton("/");
+    bar.setActionCommand(actionCommand:"bar");
+    bar.addActionListener(this);
     c.gridx = 2;
     c.gridy = 7;
     content.add(bar, c);
 
     equals = new JButton("=");
-    equals.setActionCommand(actionCommand:"=");
+    equals.setActionCommand(actionCommand:"equals");
     equals.addActionListener(this);
     c.gridx = 3;
     c.gridy = 7;
