46796d1da9bef5904f744677e09c03ea3f966df4
hooncx <148787816+hooncx@users.noreply.github.com>
2023-11-11 08:39:12 -0500

    Update CalculatorMachine.java

--- src/Operations/CalculatorMachine.java
+++ src/Operations/CalculatorMachine.java
@@ -23,44 +23,7 @@ public class CalculatorMachine implements Runnable                              
   public void run()
   {
     CalculatorWindow window = new CalculatorWindow();
-    window.setTitle("ATM");        
-    window.setVisible(true);
-    
-    //window.add
-    
-  }
-  
-  private JMenuBar setupMenu() {
- // Create a menu bar
-    JMenuBar menuBar = new JMenuBar();
-
-    // Create File menu with Exit item
-    JMenu fileMenu = new JMenu("File");
-    JMenuItem exitItem = new JMenuItem("Exit");
-    exitItem.addActionListener(e -> System.exit(0)); // Close the application on selecting Exit
-    fileMenu.add(exitItem);
-
-    // Create View menu with Pie Chart item
-    JMenu viewMenu = new JMenu("View");
-    JMenuItem pieChartItem = new JMenuItem("Pie Chart");
-    // TODO: add an action listener to pieChartItem
-    viewMenu.add(pieChartItem);
-
-    // Create Help menu with About and Help items
-    JMenu helpMenu = new JMenu("Help");
-    JMenuItem aboutItem = new JMenuItem("About");
-    
-    JMenuItem helpItem = new JMenuItem("Help");
-    // TODO: add an action listener to helpItem
-    helpMenu.add(aboutItem);
-    helpMenu.add(helpItem);
-
-    // Add menus to menu bar
-    menuBar.add(fileMenu);
-    menuBar.add(viewMenu);
-    menuBar.add(helpMenu);
-
-    return menuBar;
-  }
-  
+    //window.setTitle("Fraction Calculator");        
+    window.setVisible(true);    
+  }  
 }
