86b1d6aa1c5aa077ebeedef00459290f62794a6b
hooncx <148787816+hooncx@users.noreply.github.com>
2023-11-11 08:41:06 -0500

    Update MyButton.java

--- src/Operations/MyButton.java
+++ src/Operations/MyButton.java
@@ -1,1 +1,34 @@
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+
+public class MyButton extends JButton
+{
+  private NumberPad container;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+  public MyButton(String text) {
+    super(text);
+  }
+  
+  public MyButton(String text, NumberPad container) {
+    super(text);
+    this.container = container;
+  }
+  
+  public Dimension getMinimumSize() {
+    return new Dimension(60, 60);
+  }
+
+  public Dimension getPreferredSize() {
+    return new Dimension(60, 60);
+  }
+  
+  public Dimension getMaximumSize() {
+    return new Dimension(60, 60);
+  }
+}
 
