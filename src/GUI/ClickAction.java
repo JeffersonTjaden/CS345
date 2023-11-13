e99eb4161a5fbdfe7f0543e3bbb974099c16a68e
hooncx <148787816+hooncx@users.noreply.github.com>
2023-11-13 17:43:11 -0500

    Add files via upload

---
+++ src/GUI/ClickAction.java
@@ -0,0 +1,19 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ClickAction extends AbstractAction
+{
+  private static final long serialVersionUID = 1L;
+  private JButton button;
+  
+  public ClickAction(JButton button) {
+    super();
+    this.button = button;
+  }
+  
+  public void actionPerformed(ActionEvent evt) {
+    button.grabFocus();
+    button.doClick(50);
+  }
+
+}
