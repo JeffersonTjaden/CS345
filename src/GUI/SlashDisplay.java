e85e4f9b07f4fc9e8425040f6ff4c678052f0df2
SeanHalloran14 <hallorsa@dukes.jmu.edu>
2023-11-19 21:18:32 -0500

    Edited Display
    It will now be much easier to implement the solidus and bar style displays. I made an abstract class called Display (extending JPanel) which the classes SlashDisplay, SolidusDisplay, and BarDisplay all extend. The SlashDisplay class should be fully operational. To switch between "display modes" all we will have do to is change the instance variable "Display display" in calculator from a Slash/Solidus/BarDisplay.

---
+++ src/GUI/SlashDisplay.java
@@ -0,0 +1,104 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import utilities.*;
+
+public class SlashDisplay extends Display {
+
+    JLabel displayExpression;
+    JLabel displayOperand;
+    
+    public SlashDisplay() {
+        setLayout(new BorderLayout());
+
+        displayExpression = new JLabel();
+        displayOperand = new JLabel();
+
+        setBackground(Color.white);
+        displayExpression.setBackground(Color.white);
+        displayOperand.setBackground(Color.white);
+
+        add(displayExpression, BorderLayout.NORTH);
+        add(displayOperand, BorderLayout.EAST);
+
+        displayExpression.setText(" ");
+
+        whole = "_";
+        numerator = "_";
+        denominator = "_";
+        signText = "";
+        signBool = true;
+        updateOperand();
+
+        setVisible(true);
+    }
+
+    //Getter and Setter Methods
+
+    public void setExpression(String errorMessage) {
+        displayExpression.setText(errorMessage);
+    }
+
+    public void setPartialExpression(IrreducedMixedFraction left, String operation) {
+        displayExpression.setText(left.toString() + operation);
+        clearOperand();
+    }
+
+    public void setEvaluatedExpression(IrreducedMixedFraction left, String operation, IrreducedMixedFraction right, IrreducedMixedFraction result) {
+        displayExpression.setText(left.toString() + operation + right.toString() + "=" + result.toString());
+    }
+
+    public void setOperand(IrreducedMixedFraction operand) {
+        whole = String.valueOf(operand.getWhole());
+        numerator = String.valueOf(operand.getNumerator());
+        denominator = String.valueOf(operand.getDenominator());
+        signBool = operand.getSign();
+        if (signBool) {
+            signText = "";
+        } else {
+            signText = "-";
+        }
+        updateOperand();
+    }
+
+    //Helper Methods
+
+    protected void clearOperand() {
+        whole = "_";
+        numerator = "_";
+        denominator = "_";
+        signText = "";
+        signBool = true;
+        currentPosition = 0;
+        updateOperand();
+      }
+
+      protected void updateOperand() {
+        String text = "";
+        String wholePart = whole + " ";
+        String numeratorPart = numerator + "/";
+        String denominatorPart = denominator;
+    
+        String focusedStyle = "<span style='background-color:#D3D3D3; color:black;'>%s</span>"; // Changed color for visibility
+    
+        switch (currentPosition % 3) {
+            case 0:
+                text = String.format(focusedStyle, wholePart) + numeratorPart + denominatorPart;
+                break;
+            case 1:
+                text = wholePart + String.format(focusedStyle, numeratorPart) + denominatorPart;
+                break;
+            case 2:
+                text = wholePart + numeratorPart + String.format(focusedStyle, denominatorPart);
+                break;
+        }
+    
+        text = "<html>" + signText + text + "</html>";
+        displayOperand.setText(text);
+    }
+
+    protected void clearExpression() {
+        displayExpression.setText(" ");
+    }      
+}
---
+++ src/GUI/SolidusDisplay.java
@@ -0,0 +1,54 @@
+package GUI;
+
+import javax.swing.*;
+
+import utilities.*;
+
+import java.awt.*;
+
+public class SolidusDisplay extends Display {
+
+    @Override
+    public void setExpression(String errorMessage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setExpression'");
+    }
+
+    @Override
+    public void setPartialExpression(IrreducedMixedFraction left, String operation) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPartialExpression'");
+    }
+
+    @Override
+    public void setEvaluatedExpression(IrreducedMixedFraction left, String operation, IrreducedMixedFraction right,
+            IrreducedMixedFraction result) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setEvaluatedExpression'");
+    }
+
+    @Override
+    public void setOperand(IrreducedMixedFraction operand) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setOperand'");
+    }
+
+    @Override
+    protected void updateOperand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateOperand'");
+    }
+
+    @Override
+    protected void clearOperand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clearOperand'");
+    }
+
+    @Override
+    protected void clearExpression() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clearExpression'");
+    }
+
+}
