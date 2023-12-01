4a9e4d986faea7c464871d784a712f4a70d08a47
Dade Buschy <buschydd@dukes.jmu.edu>
2023-12-01 10:31:21 -0500

    style

--- src/GUI/Calculator.java
+++ src/GUI/Calculator.java
@@ -49,7 +49,7 @@ public class Calculator extends JFrame implements ActionListener, ComponentListe
 {
   private JPanel content = (JPanel) getContentPane();
   private GridBagConstraints c = new GridBagConstraints();
-  private Display display = new BarDisplay();;
+  private Display display = new BarDisplay();
   
   private ResourceBundle messages;
   
@@ -525,17 +525,20 @@ public class Calculator extends JFrame implements ActionListener, ComponentListe
             canCreatePieChart = true;
             break;          
           case "/":
-          if (right.getWhole() != 0 || right.getNumerator() != 0) {
-            result = Operations.divide(leftTemp, rightTemp);
-            pieChartOps.clear();
-            pieChartOps.add(left);
-            pieChartOps.add(right);
-            pieChartOps.add(result);
-            pieChartOps.add("÷");
-            canCreatePieChart = true;
-          } else {
-            display.setErrorMessage("Get a load of this silly goose dude, somebody feed him some bread he goin crazy");
-          }
+            if (right.getWhole() != 0 || right.getNumerator() != 0) 
+            {
+              result = Operations.divide(leftTemp, rightTemp);
+              pieChartOps.clear();
+              pieChartOps.add(left);
+              pieChartOps.add(right);
+              pieChartOps.add(result);
+              pieChartOps.add("÷");
+              canCreatePieChart = true;
+            } else 
+            {
+              display.setErrorMessage("Get a load of this silly goose dude,"
+                  + " somebody feed him some bread he goin crazy");
+            }
             break;         
           case "power":
             if (right.getSign()) 
