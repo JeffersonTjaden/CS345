28a1cfd66bbce0bcd06d4680ea99f47dcd3704e4
SeanHalloran14 <hallorsa@dukes.jmu.edu>
2023-12-08 17:06:15 -0500

    I don't think I changed anything but the thingy says I did

---
+++ src/GUI/Display/BarDisplay.java
@@ -0,0 +1,142 @@
+package GUI.Display;
+
+import javax.swing.*;
+
+
+import java.awt.*;
+import utilities.*;
+
+/**
+ * The BarDisplay class is for displaying the operand being inputted, the partial current expression, and the evaluated current expression in bar style.
+ * 
+ * This work complies with the JMU Honor Code.
+ * 
+ * @author Sean Halloran
+ * @version 11/28/2023
+ */
+public class BarDisplay extends Display {
+
+    private JPanel displayExpression;
+    private JPanel displayOperand;
+
+    private JLabel leftWhole;
+    private JLabel leftFraction;
+
+    private JLabel rightWhole;
+    private JLabel rightFraction;
+
+    private JLabel resultWhole;
+    private JLabel resultFraction;
+
+    private JLabel operandWhole;
+    private JLabel operandFraction;
+
+    /**
+     * Constructs a new BarDisplay with an empty current operand and an empty current expression.
+     */
+    public BarDisplay() {
+        displayExpression = new JPanel();
+        displayOperand = new JPanel();
+
+        leftWhole = new JLabel();
+        leftFraction = new JLabel();
+        rightWhole = new JLabel();
+        rightFraction = new JLabel();
+        resultWhole = new JLabel();
+        resultFraction = new JLabel();
+        operandWhole = new JLabel();
+        operandFraction = new JLabel();
+
+        setLayout(new BorderLayout());
+        displayExpression.setLayout(new FlowLayout());
+        displayExpression.setAlignmentX(FlowLayout.LEFT);
+        displayOperand.setLayout(new FlowLayout());
+
+        add(displayExpression, BorderLayout.NORTH);
+        add(displayOperand, BorderLayout.EAST);
+
+        displayExpression.add(leftWhole);
+        displayExpression.add(leftFraction);
+        displayExpression.add(rightWhole);
+        displayExpression.add(rightFraction);
+        displayExpression.add(resultWhole);
+        displayExpression.add(resultFraction);
+
+        displayOperand.add(operandWhole);
+        displayOperand.add(operandFraction);
+
+        setBackground(Color.white);
+        displayExpression.setBackground(getBackground());
+        displayOperand.setBackground(getBackground());
+
+        whole = "_";
+        numerator = "_";
+        denominator = "_";
+        signText = "";
+        signBool = true;
+        updateOperand();
+    }
+
+    @Override
+    public void setErrorMessage(String errorMessage) {
+        
+    }
+
+    @Override
+    public void setPartialExpression(IrreducedMixedFraction left, String operation) {
+        clearExpression();
+        leftWhole.setText(left.toWholeBarString() + " ");
+        leftFraction.setText(left.toFractionBarString());
+        rightWhole.setText(" " + operation);
+        clearOperand();
+    }
+
+    @Override
+    public void setEvaluatedExpression(IrreducedMixedFraction right,
+            IrreducedMixedFraction result) {
+        
+        rightWhole.setText(rightWhole.getText() + " " + right.toWholeBarString() + " ");
+        rightFraction.setText(right.toFractionBarString());
+
+        resultWhole.setText(" = " + result.toWholeBarString() + " ");
+        resultFraction.setText(result.toFractionBarString());
+    }
+
+    @Override
+    public void setEvaluatedExpression(IrreducedMixedFraction right, boolean result) {
+        rightWhole.setText(rightWhole.getText() + " " + right.toWholeBarString() + " ");
+        rightFraction.setText(right.toFractionBarString());
+
+        resultWhole.setText(" = " + Boolean.toString(result));
+    }
+
+    @Override
+    protected void updateOperand() {
+        String focusedStyle = "<span style='background-color:#D3D3D3; color:black;'>%s</span>";
+        switch (currentPosition % 3) {
+            case 0:
+                operandWhole.setText("<html>" + signText + String.format(focusedStyle, whole) + " </html>");
+                operandFraction.setText("<html><div style='text-align: center'>" + numerator + "<hr/>" + denominator + "</div></html>");
+                break;
+            case 1:
+                operandWhole.setText("<html>" + signText + whole + " </html>");
+                operandFraction.setText("<html><div style='text-align: center'>" + String.format(focusedStyle, numerator) + "<hr/>" + denominator + "</div></html>");
+                break;
+            case 2:
+                operandWhole.setText("<html>" + signText + whole + " </html>");
+                operandFraction.setText("<html><div style='text-align: center'>" + numerator + "<hr/>" + String.format(focusedStyle, denominator) + "</div></html>");
+                break;
+        }
+    }
+
+    @Override
+    protected void clearExpression() {
+        leftWhole.setText(" ");
+        leftFraction.setText("");
+        rightWhole.setText("");
+        rightFraction.setText("");
+        resultWhole.setText("");
+        resultFraction.setText("");
+    }
+    
+}
---
+++ src/GUI/Display/SolidusDisplay.java
@@ -0,0 +1,99 @@
+package GUI.Display;
+
+import javax.swing.*;
+
+import utilities.*;
+
+import java.awt.*;
+
+/**
+ * The SolidusDisplay class is for displaying the operand being inputted, the partial current expression, and the evaluated current expression in solidus style.
+ * 
+ * This work complies with the JMU Honor Code.
+ * 
+ * @author Sean Halloran
+ * @version 11/28/2023
+ */
+public class SolidusDisplay extends Display {
+
+    private JLabel displayExpression;
+    private JLabel displayOperand;
+
+    /**
+     * Constructs a new SolidusDisplay with an empty current operand and an empty current expression.
+     */
+    public SolidusDisplay() {
+        setLayout(new BorderLayout());
+
+        displayExpression = new JLabel();
+        displayOperand = new JLabel();
+
+        setBackground(Color.white);
+        displayExpression.setBackground(Color.white);
+        displayOperand.setBackground(Color.white);
+
+        add(displayExpression, BorderLayout.NORTH);
+        add(displayOperand, BorderLayout.EAST);
+
+        displayExpression.setText(" ");
+
+        whole = "_";
+        numerator = "_";
+        denominator = "_";
+        signText = "";
+        signBool = true;
+        updateOperand();
+    }
+
+    @Override
+    public void setErrorMessage(String errorMessage) {
+        displayExpression.setText(errorMessage);
+    }
+
+    @Override
+    public void setPartialExpression(IrreducedMixedFraction left, String operation) {
+        displayExpression.setText("<html>" + left.toSolidusString() + operation + "</html>");
+        clearOperand();
+    }
+
+    @Override
+    public void setEvaluatedExpression(IrreducedMixedFraction right, IrreducedMixedFraction result) {
+        displayExpression.setText(displayExpression.getText().substring(0, displayExpression.getText().length() - 7) + right.toSolidusString() + "=" + result.toSolidusString() + "</html>");
+    }
+
+    @Override
+    public void setEvaluatedExpression(IrreducedMixedFraction right, boolean result) {
+        displayExpression.setText(displayExpression.getText().substring(0, displayExpression.getText().length() - 7) + right.toSolidusString() + "=" + Boolean.toString(result));
+    }
+
+    @Override
+    protected void updateOperand() {
+        String text = "";
+        String wholePart = whole + " ";
+        String numeratorPart = "<sup>" + numerator + "</sup>" + "/";
+        String denominatorPart = "<sub>" + denominator + "</sub>";
+    
+        String focusedStyle = "<span style='background-color:#D3D3D3; color:black;'>%s</span>"; // Changed color for visibility
+    
+        switch (currentPosition % 3) {
+            case 0:
+                text = String.format(focusedStyle, wholePart) + numeratorPart + denominatorPart;
+                break;
+            case 1:
+                text = wholePart + String.format(focusedStyle, numeratorPart) + denominatorPart;
+                break;
+            case 2:
+                text = wholePart + numeratorPart + String.format(focusedStyle, denominatorPart);
+                break;
+        }
+    
+        text = "<html>" + signText + text + "</html>";
+        displayOperand.setText(text);
+    }
+
+    @Override
+    protected void clearExpression() {
+        displayExpression.setText(" ");
+    }
+
+}
